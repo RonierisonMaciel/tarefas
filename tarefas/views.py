94d487e246db9ac1cc00a5d814d4926821a1031e
ronierison.maciel <ronierison.maciel@gmail.com>
2024-10-28 21:39:47 -0300

    :fire: add urls and view app

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def lista_tarefas(request):
+    tarefas = ['Sistemas de Informação', 'Curso de Django', 'Curso de Python']
+    return render(request, 'lista_tarefas.html', {'tarefas': tarefas})
