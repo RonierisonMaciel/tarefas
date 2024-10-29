d216d804243bb1375c38b2e3a7c1fb95bf8f1eb5
ronierison.maciel <ronierison.maciel@gmail.com>
2024-10-28 22:20:52 -0300

    :fire: add all

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
+from .models import Tarefa
 # Create your views here.
 
 def lista_tarefas(request):
-    tarefas = ['Sistemas de Informação', 'Curso de Django', 'Curso de Python']
+    tarefas = Tarefa.objects.all()
     return render(request, 'lista_tarefas.html', {'tarefas': tarefas})
