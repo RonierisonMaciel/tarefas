4142a549424816545b9531da7c96e5ec3274e209
ronierison.maciel <ronierison.maciel@gmail.com>
2024-11-04 16:23:35 -0300

    :bug: :zap: remove unnecessary fields from Tarefa model and update migration; enhance task list template and view logic

--- tarefas/models.py
+++ tarefas/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 class Tarefa(models.Model):
     titulo = models.CharField(max_length=100)
-    descricao = models.TextField(blank=True, null=True)
-    data_criacao = models.DateTimeField(auto_now_add=True)
     concluida = models.BooleanField(default=False)
 
     def __str__(self):
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Tarefa
 # Create your views here.
 
 def lista_tarefas(request):
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        if titulo:
+            Tarefa.objects.create(titulo=titulo)
+            return redirect('lista_tarefas')
+
     tarefas = Tarefa.objects.all()
     return render(request, 'lista_tarefas.html', {'tarefas': tarefas})
