d23f655e812f31a82a2de901bdc0635eba2938aa
ronierison.maciel <ronierison.maciel@gmail.com>
2024-11-11 20:30:59 -0300

    remove Atividade model from tarefas app

--- tarefas/models.py
+++ tarefas/models.py
@@ -8,8 +8,3 @@ class Tarefa(models.Model):
 
     def __str__(self):
         return self.titulo
-    
-class Atividade(models.Model):
-    nome = models.CharField(max_length=100)
-    idade = models.IntegerField()
-    
