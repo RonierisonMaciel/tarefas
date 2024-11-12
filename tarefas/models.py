03217a8a257e4ffae6f252c36de948bec682ac2e
ronierison.maciel <ronierison.maciel@gmail.com>
2024-11-11 22:16:56 -0300

    :zap: add validation to prevent duplicate task titles and set default ordering for tasks

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,5 +1,7 @@
+# tarefas/models.py
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class Tarefa(models.Model):
     titulo = models.CharField(max_length=100)
@@ -8,3 +10,10 @@ class Tarefa(models.Model):
 
     def __str__(self):
         return self.titulo
+    
+    class Meta:
+        ordering = ['-id']  # Exemplo: ordenar tarefas pela mais recente
+
+    def clean(self):
+        if Tarefa.objects.filter(titulo=self.titulo).exclude(id=self.id).exists():
+            raise ValidationError('Você já possui uma tarefa com este título.')
